e4812c41e73143f84dc9f433e752d80b06d71693
Jae Basagre <basagrejae@gmail.com>
2025-05-20 11:18:32 +0800

    Calculator in PHP

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 
     // $a = "Hello";
     // $b = "World";
-    // $c = $a . " " . $b;
+    // $c = $a . " " . $b; // Concatenation operator
     // echo $c;
 
     // Arithmetic operator
@@ -206,6 +206,70 @@
         </form>
     </main> -->
    
+
+    
+    <!-- <form action="<?php echo htmlspecialchars($_SERVER
+    ["PHP_SELF"]); ?>" method="POST">
+        <input type="number" name="num01" placeholder="Number one" required>
+        <select name="operator"> 
+            <option value="add">+</option>
+            <option value="subtract">-</option>
+            <option value="multiply">*</option>
+            <option value="divide">/</option>
+        </select>
+        <input type="number" name="num02" placeholder="Number two" required>
+        <button>Calculate</button>
+    </form> -->
+    
+    
+    <?php
+    // This is a Calculator in PHP
+
+    // if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //     // Grab data from the form
+    //     $num01 = filter_input(INPUT_POST, "num01", FILTER_SANITIZE_NUMBER_FLOAT); // Sanitize the input
+    //     $num02 = filter_input(INPUT_POST, "num02", FILTER_SANITIZE_NUMBER_FLOAT); 
+    //     $operator = htmlspecialchars($_POST["operator"]); // Sanitize the input
+
+    //     // Error Handlers
+    //     $errors = false;
+
+    //     if (empty($num01) || empty($num02) || empty($operator)) { // Check if any field is empty
+    //         echo "<p class='calc-error'>Fill in all fields!</p>"; // Display error message
+    //         $errors = true;
+    //     }
+    //     if (!is_numeric($num01) || !is_numeric($num02)) { // Check if the inputs are numbers
+    //         echo "<p class='calc-error'>Only write numbers!</p>";
+    //         $errors = true;
+    //     }
+
+    //     //Calculate the numbers if no errors
+    //     if (!$errors) {
+    //         $value = 0; // Initialize the value variable or default value
+
+    //         switch ($operator) {
+    //             case "add":
+    //                 $value = $num01 + $num02;
+    //                  break;
+    //             case "subtract":
+    //                 $value = $num01 - $num02;
+    //                 break;
+    //             case "multiply":
+    //                 $value = $num01 * $num02;
+    //                 break;
+    //             case "divide":
+    //                 $value = $num01 / $num02;
+    //                 break;
+    //             default:
+    //                 echo "<p class='calc-error'>Something went wrong!</p>";
+    //         }
+
+    //         echo "<p class='calc-result'>Result = " .
+    //         $value . "</p>";
+    //     }
+    // }
+    
+    ?> 
     
         <!-- <p> Hi! My name is  
     <?php
