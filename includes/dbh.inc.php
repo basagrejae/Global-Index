e64eda84a23cd66d3a5adf6eda2bc3ece368aff0
Jae Basagre <basagrejae@gmail.com>
2025-05-28 08:54:09 +0800

    Database connection

---
+++ includes/dbh.inc.php
@@ -0,0 +1,13 @@
+<?php 
+
+// Database connection
+$dsn = "msql:host=localhost;dbname=myfirstdatabase";
+$dbUsername = "root";
+$dbPassword = "";
+
+try {
+    $pdo = new PDO($dsn, $dbUsername, $dbPassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
